a401465de64da92b48e1b412ceb15dc18d680bce
claudeshamie <claudeshamie@gmail.com>
2023-07-10 04:18:04 -0700

    0x01-python_async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import asyncio
 import random
 
@@ -6,3 +7,10 @@ async def wait_random(max_delay=10):
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+async def main():
+    delay = await wait_random()
+    print(f"The random delay is: {delay} seconds")
+
+# Create an event loop and run the main coroutine
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
